1ed3a532bb55ada31548ebf991983b888cd18192
ShubhamSinghTX <singh.shubham@testingxperts.com>
2024-03-05 18:52:52 +0530

    Search Functionality TASK: 781

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -15,12 +15,23 @@ const ProductList = () => {
     const fontfamily = { fontFamily: "Times New Roman" };
     const borderHello = { border: "none" };
     const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
 
     useEffect(() => {
         fetchCategorylist();
         dispatch(fetchProducts(''));
         document.title = "Our Products";
+        console.log(searchTerm)
+        //   filterProductsBySearchTerm(searchTerm)
     }, []);
+
+    useEffect(() => {
+        setFilteredProducts(
+            filterProductsBySearchTerm(allproducts, searchTerm)
+        );
+    }, [searchTerm, allproducts]);
 
     const fetchCategorylist = async () => {
         setLoading(true);
@@ -30,20 +41,30 @@ const ProductList = () => {
             setLoading(false);
         }
     }
+
     const fetchProductCategorylist = (id) => {
         let filterCategoryName = dropdown.filter(x => x.id == id).map(x => x.Category)[0];
+        setFilteredProducts(filterProductsBySearchTerm(allproducts, searchTerm));
         setSelectedValue(filterCategoryName);
         dispatch(fetchProducts(id));
     }
 
+    const filterProductsBySearchTerm = (products, searchTerm) => {
+        return products.filter((product) =>
+            product.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    };
+
+    const SearchTerm = (e) => {
+        setSearchTerm(e)
+    }
     return (
         <>
             <LoadingOverlay active={loading} spinner text='Loading...'>
                 <div className="py-5">
                     <div className="container">
                         <div className="row">
-                            <div className="container-fluid">
-
+                            <div className="container-fluid d-flex justify-content-between">
                                 <Dropdown title="All Category" onSelect={(e) => fetchProductCategorylist(e)}>
                                     <Dropdown.Toggle id="dropdown-basic" className='tx-dropdown' style={{ background: 'rgba(243, 243, 243, 0.24', backdropFilter: '20px', boxShadow: 'rgba(0, 0, 0, 0.05) 1px 1px 10px 0px', ...borderHello, color: '#053645' }}>
                                         <strong>
@@ -57,17 +78,21 @@ const ProductList = () => {
                                         ))}
                                     </Dropdown.Menu>
                                 </Dropdown>
+
+                                <input type='text'
+                                    className='searchbar-input'
+                                    value={searchTerm}
+                                    onChange={(e) => SearchTerm(e.target.value)}
+                                    placeholder='&#128269; Search your product...' />
                             </div>
                             <div className='container'>
                                 <div className='mt-1 row'>
                                     {
-                                        allproducts.map(product => {
-                                            return (
-                                                <Link to="/details" className='my-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 product-card'>
-                                                    <Product key={product.id} product={product} />
-                                                </Link>
-                                            );
-                                        })
+                                        filteredProducts.map((product) => (
+                                            <Link to='/details' className='my-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 product-card' key={product.id}>
+                                                <Product product={product} />
+                                            </Link>
+                                        ))
                                     }
                                 </div>
                             </div>
